eccd993cf1d3e5d55cf470f716484c36e078bc81
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-15 13:41:05 +0100

    created form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 import styled from '@emotion/styled';
 
@@ -19,7 +20,7 @@ function App() {
       <Header titulo='Cotizador de seguros'/>
 
       <ContenedorFormulario>
-        
+        <Formulario />
       </ContenedorFormulario>
     </Contenedor>    
   );
