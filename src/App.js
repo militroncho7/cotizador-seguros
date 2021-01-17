614627ccbc17415399e50454ead00dccea468d71
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-01-17 11:27:22 +0100

    mostrando el total a pagar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Resumen from './components/Resumen';
+import Resultado from './components/Resultado';
 
 import styled from '@emotion/styled';
 
@@ -27,7 +28,7 @@ function App() {
   });
 
   //Extraer datos
-  const { datos } = resumen;
+  const { cotizacion, datos } = resumen;
 
   return (
     <Contenedor>
@@ -40,6 +41,10 @@ function App() {
 
         <Resumen
           datos={datos}
+        />
+
+        <Resultado
+          cotizacion={cotizacion}
         />
 
       </ContenedorFormulario>
